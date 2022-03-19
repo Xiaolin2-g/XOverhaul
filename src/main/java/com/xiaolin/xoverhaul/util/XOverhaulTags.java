184e689e918bae0784f579e58b4b38823465944d
Xiaolin2 <xiaolingaturro@gmail.com>
2022-03-19 04:41:40 -0300

    fixed tags

--- src/main/java/com/xiaolin/xoverhaul/util/XOverhaulTags.java
+++ src/main/java/com/xiaolin/xoverhaul/util/XOverhaulTags.java
@@ -7,26 +7,28 @@ import net.minecraft.item.Item;
 import net.minecraft.tag.BlockTags;
 import net.minecraft.tag.ItemTags;
 import net.minecraft.tag.Tag;
+import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.lwjgl.system.CallbackI;
 
 public class XOverhaulTags {
 
     public static class Blocks {
 
-        public static final Tag.Identified<Block> ENDER_FLOWER_PLACEABLES = TagFactory.BLOCK.create(new Identifier(
+        public static final TagKey<Block> ENDER_FLOWER_PLACEABLES = TagKey.of(Registry.BLOCK_KEY, new Identifier(
                 XOverhaul.MOD_ID, "ender_flower_placeables"));
 
-        public static final Tag.Identified<Block> GOLDEN_PRISMARINE_BRICK_CAN_REPLACE = TagFactory.BLOCK.create(new Identifier(
+        public static final TagKey<Block> GOLDEN_PRISMARINE_BRICK_CAN_REPLACE = TagKey.of(Registry.BLOCK_KEY, new Identifier(
                 XOverhaul.MOD_ID, "golden_prismarine_brick_can_replace"));
 
-        public static final Tag.Identified<Block> TRIPWIRE_HOOKS = TagFactory.BLOCK.create(new Identifier(
+        public static final TagKey<Block> TRIPWIRE_HOOKS = TagKey.of(Registry.BLOCK_KEY, new Identifier(
                 XOverhaul.MOD_ID, "tripwire_hooks"));
     }
 
     public static class Items{
 
-        public static final Tag.Identified<Item> TRIPWIRE_HOOKS = TagFactory.ITEM.create(new Identifier(
+        public static final TagKey<Item> TRIPWIRE_HOOKS = TagKey.of(Registry.ITEM_KEY, new Identifier(
                 XOverhaul.MOD_ID, "tripwire_hooks"));
     }
 

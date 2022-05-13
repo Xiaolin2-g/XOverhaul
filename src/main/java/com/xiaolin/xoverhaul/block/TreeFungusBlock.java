ec2bb3cfcf95989b59e085152684423b6ba4ca42
Xiaolin2 <xiaolingaturro@gmail.com>
2022-05-13 16:49:53 -0300

    fallen log

--- src/main/java/com/xiaolin/xoverhaul/block/TreeFungusBlock.java
+++ src/main/java/com/xiaolin/xoverhaul/block/TreeFungusBlock.java
@@ -1,25 +1,50 @@
 package com.xiaolin.xoverhaul.block;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.ShapeContext;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import net.minecraft.block.*;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.Property;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 
 public class TreeFungusBlock extends Block {
+
+
+    public static final DirectionProperty FACING;
+
+    private final VoxelShape SHAPE = VoxelShapes.combineAndSimplify(Block.createCuboidShape(0, 0, 0, 16, 2, 11), Block.createCuboidShape(3, 0, 1, 13, 2, 14), BooleanBiFunction.OR);
+
     public TreeFungusBlock(Settings settings) {
         super(settings);
     }
 
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return VoxelShapes.combineAndSimplify
-                        (Block.createCuboidShape(0, 0, 2.5, 16, 2, 13.5),
-                                Block.createCuboidShape(3, 0, 0, 13, 2, 16),
-                                BooleanBiFunction.OR);
+        return SHAPE;
+    }
+
+    public BlockState rotate(BlockState state, BlockRotation rotation) {
+        return state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    public BlockState mirror(BlockState state, BlockMirror mirror) {
+        return state.rotate(mirror.getRotation(state.get(FACING)));
+    }
+
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    static {
+        FACING = HorizontalFacingBlock.FACING;
     }
 
 }

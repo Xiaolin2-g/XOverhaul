80152939dee41854a07966c9119a1deccf9bb93a
Xiaolin2 <xiaolingaturro@gmail.com>
2022-08-11 03:36:29 -0300

    Leatherling and Chained Monster no longer spawn on caves

--- src/main/java/com/xiaolin/xoverhaul/entity/ModEntities.java
+++ src/main/java/com/xiaolin/xoverhaul/entity/ModEntities.java
@@ -1,6 +1,7 @@
 package com.xiaolin.xoverhaul.entity;
 
 import com.xiaolin.xoverhaul.XOverhaul;
+import com.xiaolin.xoverhaul.entity.entities.BaseHostileEntity;
 import com.xiaolin.xoverhaul.entity.entities.ChainedMonsterEntity;
 import com.xiaolin.xoverhaul.entity.entities.LeatherlingEntity;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
@@ -8,6 +9,7 @@ import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.Heightmap;
 
 public class ModEntities {
 
@@ -17,35 +19,42 @@ public class ModEntities {
 
     public static final EntityType<ChainedMonsterEntity> CHAINED_MONSTER =
             ModEntities.registerEntity(CHAINED_MONSTER_ID, SpawnGroup.MONSTER,
-                    ChainedMonsterEntity::new, 1.95f, 0.6f);
+                    ChainedMonsterEntity::new, 1.95f, 0.6f,
+                    SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
+                    BaseHostileEntity::canSpawnInSurfaceAndDarkness);
 
     public static final EntityType<LeatherlingEntity> LEATHERLING =
             ModEntities.registerEntity(LEATHERLING_ID, SpawnGroup.MONSTER,
-                    LeatherlingEntity::new, 1.2f, 0.6f);
+                    LeatherlingEntity::new, 1.2f, 0.6f,
+                    SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
+                    BaseHostileEntity::canSpawnInSurface);
 
 
     public static EntityType registerEntity(String name, SpawnGroup spawnGroup,
                                             EntityType.EntityFactory entityFactory,
-                                            float height, float width){
+                                            float height, float width,
+                                            SpawnRestriction.Location location, Heightmap.Type heightmap,
+                                            SpawnRestriction.SpawnPredicate spawnPredicate){
 
-        return Registry.register(Registry.ENTITY_TYPE,
-                new Identifier(XOverhaul.MOD_ID, name),
-                FabricEntityTypeBuilder.create(
-                        spawnGroup, entityFactory)
+        return Registry.register(Registry.ENTITY_TYPE, new Identifier(XOverhaul.MOD_ID, name),
+                FabricEntityTypeBuilder.Living.createMob()
+                        .spawnGroup(spawnGroup)
+                        .entityFactory(entityFactory)
                         .dimensions(EntityDimensions.fixed(width, height))
+                        .spawnRestriction(location, heightmap, spawnPredicate)
                         .build());
     }
 
     public static void registerModEntities(){
         XOverhaul.LOGGER.info("Registering Mod Entities for " + XOverhaul.MOD_ID);
-
         registerMobAttributes();
-
     }
 
     public static void registerMobAttributes(){
         FabricDefaultAttributeRegistry.register(CHAINED_MONSTER, ChainedMonsterEntity.createAttributes());
         FabricDefaultAttributeRegistry.register(LEATHERLING, LeatherlingEntity.createAttributes());
-
     }
+
+
+
 }
--- src/main/java/com/xiaolin/xoverhaul/entity/entities/BaseHostileEntity.java
+++ src/main/java/com/xiaolin/xoverhaul/entity/entities/BaseHostileEntity.java
@@ -2,13 +2,20 @@ package com.xiaolin.xoverhaul.entity.entities;
 
 import com.xiaolin.xoverhaul.util.AnimationHelper;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.LightType;
+import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
+
+import java.util.Random;
 
 public class BaseHostileEntity extends HostileEntity implements IAnimatable {
 
@@ -17,6 +24,28 @@ public class BaseHostileEntity extends HostileEntity implements IAnimatable {
     protected BaseHostileEntity(EntityType<? extends HostileEntity> entityType, World world) {
         super(entityType, world);
     }
+
+    public static boolean isSpawnSurface(ServerWorldAccess world, BlockPos pos, Random random) {
+        return world.getLightLevel(LightType.SKY, pos) > 0;
+    }
+
+    public static boolean isSpawnSurfaceAndDarkness(ServerWorldAccess world, BlockPos pos, Random random) {
+        if (world.getLightLevel(LightType.BLOCK, pos) > 0) {
+            return false;
+        }
+        int i = world.toServerWorld().isThundering() ? world.getLightLevel(pos, 10) : world.getLightLevel(pos);
+        return i <= random.nextInt(8);
+    }
+
+    public static boolean canSpawnInSurface(EntityType<? extends HostileEntity> type, ServerWorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random) {
+        return world.getDifficulty() != Difficulty.PEACEFUL && isSpawnSurface(world, pos, random) && HostileEntity.canMobSpawn(type, world, spawnReason, pos, random);
+    }
+
+    public static boolean canSpawnInSurfaceAndDarkness(EntityType<? extends HostileEntity> type, ServerWorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random) {
+        return canSpawnInSurface(type, world, spawnReason, pos, random) && isSpawnSurfaceAndDarkness(world, pos, random);
+    }
+
+
 
     public void initGoals() {
 
